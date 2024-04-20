74ca64dffa91da2fe89442d01699a623bf09e044
Harmeet Singh <hskhanduja03@gmail.com>
2024-04-20 11:49:36 +0530

    third

---
+++ script.js
@@ -0,0 +1,42 @@
+var tl = gsap.timeline()
+
+// tl.from("#nav #logo", {
+//     y:"-100%",
+//     opacity:0,
+//     duration:1,
+//     delay:0.2,
+// })
+
+// tl.from("#nav a", {
+//     y:"-100%",
+//     opacity:0,
+//     duration:1,
+//     stagger:0.1
+// })
+
+// tl.from("#nav button", {
+//     y:"-100%",
+//     opacity:0,
+//     duration:1,
+//     stagger:0.1
+// })
+
+tl.from("#nav .gs", {
+    y:"-100%",
+    opacity:0,
+    duration:1,
+    delay:0.3,
+    stagger:0.1
+})
+
+tl.from("#hero img", {
+    scale:0.8,
+    duration:1,
+    opacity:0
+})
+
+tl.from(".btn", {
+    y: "0px", // Adjust the value as needed
+    opacity: 0,
+    duration: 0.5,
+});
